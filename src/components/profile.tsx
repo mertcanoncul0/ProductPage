1ff4683712cb209dfcbc3d30e614337a33b8a3bf
Mertcan Öncül <mertcanoncul@icloud.com>
2024-08-31 04:09:28 +0300

    fixed dropdown for responsive

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -17,7 +17,7 @@ export function Profile() {
         className={`header-actions__profile${profile ? " active" : ""}`}
         onClick={handleProfileState}
       >
-        <img src="/images/image-avatar.png" alt="" />
+        <img src="/images/image-avatar.png" alt="profile icon" />
       </button>
 
       <div
