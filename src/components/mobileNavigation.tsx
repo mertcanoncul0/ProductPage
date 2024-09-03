57a362a49ff266424f5be993532925e1c1f0096c
Mertcan Öncül <mertcanoncul@icloud.com>
2024-09-03 15:12:46 +0300

    mobile menu icon changed

--- src/components/mobileNavigation.tsx
+++ src/components/mobileNavigation.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "preact/hooks"
 import { navigations } from "../constants"
+import { MenuIcon } from "./menu-icon"
 
 export function MobileNavigation() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
@@ -27,36 +28,14 @@ export function MobileNavigation() {
     }
   }, [isDrawerOpen])
 
-  const openDrawer = () => setIsDrawerOpen(true)
-  const closeDrawer = () => setIsDrawerOpen(false)
+  const toggleDrawer = () => setIsDrawerOpen((prev) => !prev)
 
   return (
     <>
-      <button
-        className="header-nav__mobile-menu-btn"
-        onClick={openDrawer}
-        aria-label="Mobile menu icon"
-      >
-        <img src="/images/icon-menu.svg" alt="Mobile menu icon" />
-      </button>
+      <MenuIcon onClick={toggleDrawer} />
       <div
         className={`header-nav__mobile-drawer${isDrawerOpen ? " show" : ""}`}
       >
-        <button
-          className="mobile-drawer__close-btn"
-          onClick={closeDrawer}
-          aria-label="Close mobile menu"
-        >
-          <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-              fill="#69707D"
-              stroke="#69707D"
-              fillRule="evenodd"
-            />
-          </svg>
-        </button>
-
         <ul className="mobile-drawer__list">
           {navigations.map((nav) => (
             <li className="mobile-drawer__item" key={nav.link}>
